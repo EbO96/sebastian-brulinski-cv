77df106f3484e954d627629f5e1f12a9e37b3f08
DESKTOP-P03RRA6\sebas <smog.powietrze@gmail.com>
2018-07-10 23:07:52 +0200

    - Created project. - Created PersonalInfo class to hold personal info

---
+++ app/src/main/java/cv/brulinski/sebastian/PersonalInfo.kt
@@ -0,0 +1,27 @@
+package cv.brulinski.sebastian
+
+/**
+ * Class to hold personal info
+ */
+class PersonalInfo {
+
+    var name = ""
+    var surname = ""
+    var birthDay = 0
+    var birthMonth = 0
+    var birthYear = 0
+
+    class Contact {
+        var phoneNumber = 0L
+        var email = ""
+    }
+
+    class Address {
+
+        var cityName = ""
+        var provinceName = ""
+        var latitude = 0.0
+        var longitude = 0.0
+
+    }
+}
